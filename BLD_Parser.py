1d53c408fe74d31802bb8894f9cd589ac9a13f5b
rotohands <rotohands@gmail.com>
2021-09-23 23:47:25 +0300

    small fixes solve txt format

--- BLD_Parser.py
+++ BLD_Parser.py
@@ -635,7 +635,7 @@ class Cube:
 
         self.url = ""
         self.name_of_solve = "{}{}{}{}{}{}".format("DNF(" if not self.success else "", self.time_solve if time != None else "", "({},{})".format(self.memo_time,self.exe_time) if self.memo_time != "" and self.exe_time != "" else "",
-                                                 ")\n" if not self.success else "", "  {}%".format(round(self.fluidness, 2)) if self.success and self.fluidness != 0 else "", "{}\n".format(time))
+                                                 ")\n" if not self.success else "", "  {}%\n".format(round(self.fluidness, 2)) if self.success and self.fluidness != 0 else "", "{}\n".format(time))
 
         solve_stats_copy = list(self.solve_stats)
         solve = "{}\nScramble:\n{}\n".format(self.name_of_solve, self.union_moves(self.scramble))
