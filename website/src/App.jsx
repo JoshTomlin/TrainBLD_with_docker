5918fbf3216eb5b6330c68a22733d5076664bf64
rotohands <rotohands@gmail.com>
2025-01-04 23:04:47 +0200

    updated fetch to trainld

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -769,7 +769,7 @@ class App extends React.Component {
     };
 
     fetch("https://trainbld.com:8080/parse", requestOptions)
-      // fetch("http://127.0.0.1:8080/parse", requestOptions)
+    // fetch("http://127.0.0.1:8080/parse", requestOptions)
       .then((response) =>
         response.json().then((data) => {
           result = data;
